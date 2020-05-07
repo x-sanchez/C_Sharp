580373732f56eb05a4bb3580d6dda35e3b083790
nik101123 <nik.zhilin.000@gmail.com>
2020-05-08 01:41:12 +0300

    registration

--- PracticeZhilin2020/HeadForm.Designer.cs
+++ PracticeZhilin2020/HeadForm.Designer.cs
@@ -28,12 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.AutorizatePanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // AutorizatePanel
+            // 
+            this.AutorizatePanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.AutorizatePanel.Location = new System.Drawing.Point(0, 0);
+            this.AutorizatePanel.Name = "AutorizatePanel";
+            this.AutorizatePanel.Size = new System.Drawing.Size(800, 100);
+            this.AutorizatePanel.TabIndex = 0;
+            // 
+            // HeadForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.AutorizatePanel);
+            this.Name = "HeadForm";
             this.Text = "HeadForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel AutorizatePanel;
     }
 }
