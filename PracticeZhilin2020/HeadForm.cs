c59a35f534398cc7dd160cbc1d25f7066e64f097
nik101123 <nik.zhilin.000@gmail.com>
2020-05-07 22:31:24 +0300

    registration DB

---
+++ PracticeZhilin2020/HeadForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracticeZhilin2020
+{
+    public partial class HeadForm : Form
+    {
+        public HeadForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
